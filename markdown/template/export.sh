e87dd9268d02ec423036a5c6cfeec1a0a1d24fe1
Tamimi <tamimi@solace.com>
2020-05-26 11:49:57 -0400

    modify export

--- markdown/template/export.sh
+++ markdown/template/export.sh
@@ -6,5 +6,7 @@
 
 # Get markdown file name
 codelab_markdown_filename=`ls *.md`
+codelab_dir=`basename $codelab_markdown_filename .md`
+rm -fr $codelab_dir
 
 claat export -ga UA-3921398-10 -o ../../codelabs/ $codelab_markdown_filename
